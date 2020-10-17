54ba9ae4f35f8f46acde8cf255e4ca2e85569042
hong1@futurewei.com <Hongwei Chen>
2020-10-16 23:07:21 -0700

    bpf_printk works

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-INC=-I/home/howell/work/linux/include -I/home/howell/work/linux/tools/perf/include
+INC=-I/home/howell/work/linux/include -I/home/howell/work/linux/tools/lib 
 
 .PHONY: clean all
 all: hwtc.o
 
 hwtc.o: hwtc.c
-	clang -O2 -g -Wall -target bpf ${INC} -c $< -o $@ 
+	clang -O2 -g -Wall -Werror -target bpf ${INC} -c $< -o $@ 
 
 clean:
 	rm -f hwtc.o
--- hwtc.c
+++ hwtc.c
@@ -1,16 +1,18 @@
 #include <uapi/linux/bpf.h>		// required by struct __sk_buff
 #include <uapi/linux/pkt_cls.h>		// required by TC_ACT_OK
-#include <bpf/bpf.h>			// required by SEC()
+#include <bpf/bpf_helpers.h>		// required by SEC, bpf_trace_printk
 
 SEC("egress")
 int tc_egress(struct __sk_buff *skb)
 {
+    bpf_printk("|||||\n");
     return TC_ACT_OK;
 }
 
 SEC("egress-block")
 int tc_egressblock(struct __sk_buff *skb)
 {
+    bpf_printk("xxxxx\n");
     return TC_ACT_SHOT;
 }
 
